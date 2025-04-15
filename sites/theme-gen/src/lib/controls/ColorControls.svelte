ba91d9258992156bbda1f5154f0d6b07d7b3e230
Kmyrrpen <shanenescarimoro@gmail.com>
2025-04-16 00:55:47 +0800

    added color generation to theme-gen

--- sites/theme-gen/src/lib/controls/ColorControls.svelte
+++ sites/theme-gen/src/lib/controls/ColorControls.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-  import chroma from "chroma-js";
-  import { app } from "$lib/app.svelte";
-  import { colorNames, colorShades } from "$lib/constants";
+  import { colorNames } from "$lib/constants";
 
   import Controls from "$lib/components/Controls.svelte";
+  import ColorPaletteControls from "./ColorPaletteControls.svelte";
 
   let colorName = $state("primary");
 </script>
@@ -29,59 +28,7 @@
     {/each}
   </div>
 
-  <h3 class="hd-6">{colorName}</h3>
-  <div class="flex flex-col gap-1">
-    {#each colorShades as colorShade}
-      {@const color = app.vars.get(
-        `--color-${colorName}-${colorShade}`
-      ) as string}
-
-      <div class="flex gap-2 items-center">
-        <label
-          class="flex items-center justify-center w-8"
-          for="color-{colorName}-{colorShade}">{colorShade}</label
-        >
-        <input
-          class="input"
-          type="color"
-          name="color-{colorName}-{colorShade}"
-          value={(() => {
-            /* 
-              color inputs only accept hex values, it's okay if oklch --> hex is lossy,
-              if this input is used, hex --> oklch is loseless
-             */
-            return chroma(color).hex();
-          })()}
-          oninput={(e) => {
-            const v = e.currentTarget.value;
-            if (chroma.valid(v)) {
-              if (v !== color) {
-                app.vars.set(`--color-${colorName}-${colorShade}`, v);
-              }
-            } else {
-              e.currentTarget.value = color;
-            }
-          }}
-        />
-        <div class="grow">
-          <input
-            class="input"
-            id="color-{colorName}-{colorShade}"
-            name="color-{colorName}-{colorShade}"
-            value={chroma(color).css("oklch")}
-            onchange={(e) => {
-              const v = e.currentTarget.value;
-              if (chroma.valid(v)) {
-                if (v !== color) {
-                  app.vars.set(`--color-${colorName}-${colorShade}`, v);
-                }
-              } else {
-                e.currentTarget.value = color;
-              }
-            }}
-          />
-        </div>
-      </div>
-    {/each}
-  </div>
+  {#key colorName}
+    <ColorPaletteControls {colorName} />
+  {/key}
 </Controls>
---
+++ sites/theme-gen/src/lib/controls/ColorPaletteControls.svelte
@@ -0,0 +1,161 @@
+<script lang="ts">
+  import chroma from "chroma-js";
+  import { app } from "$lib/app.svelte";
+  import { colorShades } from "$lib/constants";
+
+  type Props = {
+    colorName: string;
+  };
+
+  const { colorName }: Props = $props();
+  let createMode: "manual" | "auto" | "tri-auto" = $state("manual");
+
+  function setAutoPalette(config: { tri?: boolean }) {
+    const lightest = app.vars.get(`--color-${colorName}-50`);
+    const middle = app.vars.get(`--color-${colorName}-500`);
+    const darkest = app.vars.get(`--color-${colorName}-950`);
+
+    if (lightest && middle && darkest) {
+      const f = chroma
+        .scale(config.tri ? [lightest, middle, darkest] : [lightest, darkest])
+        .mode("oklch");
+
+      for (let i = 0; i < colorShades.length; i++) {
+        const colorShade = colorShades[i];
+        app.vars.set(
+          `--color-${colorName}-${colorShade}`,
+          chroma(f(i / (colorShades.length - 1))).css("oklch")
+        );
+      }
+    }
+  }
+
+  function onColorChange(key: string, color: string, input: HTMLInputElement) {
+    const value = input.value;
+    if (!chroma.valid(value)) {
+      input.value = color;
+      return;
+    }
+
+    app.vars.set(key, value);
+
+    if (createMode !== "manual") {
+      setAutoPalette({ tri: createMode === "tri-auto" });
+    }
+  }
+</script>
+
+<div class="space-y-2">
+  <h3 class="hd-6">{colorName}</h3>
+  <div class="flex items-center gap-4">
+    <label class="flex items-center gap-1">
+      <input
+        type="radio"
+        class="radio"
+        name="color-{colorName}-create-mode"
+        bind:group={createMode}
+        value="manual"
+      />
+      <span class="label-text">Manual</span>
+    </label>
+    <label class="flex items-center gap-1">
+      <input
+        type="radio"
+        class="radio"
+        name="color-{colorName}-create-mode"
+        bind:group={createMode}
+        value="auto"
+      />
+      <span class="label-text">Auto</span>
+    </label>
+    <label class="flex items-center gap-1">
+      <input
+        type="radio"
+        class="radio"
+        name="color-{colorName}-create-mode"
+        bind:group={createMode}
+        value="tri-auto"
+      />
+      <span class="label-text">Tri-Auto</span>
+    </label>
+  </div>
+
+  <p class="form-field-caption">
+    {#if createMode === "manual"}
+      You can edit each shade manually, switch to auto if you want to
+      automatically interpolate between two colors.
+    {:else if createMode === "auto"}
+      shades between the lightest (50) and the darkest (950) are automatically
+      generated. Switch to manual if you want to set them yourself.
+    {:else if createMode === "tri-auto"}
+      shades between the lightest (50), the middle (500), and the darkest (950)
+      are automatically generated. Switch to manual if you want to set them
+      yourself.
+    {/if}
+  </p>
+</div>
+
+{#if createMode !== "manual"}
+  <div class="flex justify-end">
+    <button
+      class="btn btn-sm filled-primary-100-900"
+      onclick={() => setAutoPalette({ tri: createMode === "tri-auto" })}
+      >regenerate</button
+    >
+  </div>
+{/if}
+
+<div class="flex flex-col gap-1">
+  {#each colorShades as colorShade}
+    {@const color = app.vars.get(
+      `--color-${colorName}-${colorShade}`
+    ) as string}
+    {@const isDisabled =
+      createMode === "auto"
+        ? colorShade > 50 && colorShade < 950
+        : createMode === "tri-auto"
+          ? colorShade !== 50 && colorShade !== 500 && colorShade !== 950
+          : false}
+
+    <div class="flex gap-2 items-center">
+      <label
+        class="flex items-center justify-center w-8"
+        for="color-{colorName}-{colorShade}">{colorShade}</label
+      >
+      <input
+        class="input"
+        type="color"
+        name="color-{colorName}-{colorShade}"
+        disabled={isDisabled}
+        value={(() => {
+          /* 
+            color inputs only accept hex values, it's okay if oklch --> hex is lossy,
+            if this input is used, hex --> oklch is loseless
+           */
+          return chroma(color).hex();
+        })()}
+        oninput={(e) =>
+          onColorChange(
+            `--color-${colorName}-${colorShade}`,
+            chroma(color).hex(),
+            e.currentTarget
+          )}
+      />
+      <div class="grow">
+        <input
+          class="input"
+          id="color-{colorName}-{colorShade}"
+          name="color-{colorName}-{colorShade}"
+          disabled={isDisabled}
+          value={chroma(color).css("oklch")}
+          onchange={(e) =>
+            onColorChange(
+              `--color-${colorName}-${colorShade}`,
+              chroma(color).css("oklch"),
+              e.currentTarget
+            )}
+        />
+      </div>
+    </div>
+  {/each}
+</div>
